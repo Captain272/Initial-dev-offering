87dd991da18e1ef470d41edf55e4d4cb480d4271
Ashok kumar <73692009+ashok49473@users.noreply.github.com>
2022-10-21 11:56:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    data_path = 'artifacts/cleaned_users.csv'
+    model_path = 'artifacts/model.pkl'
+    scaler_path = 'artifacts/scaler.pkl'
+    vectorizer_path = 'artifacts/vectorizer.pkl'
+
+
+
+config = Config()
